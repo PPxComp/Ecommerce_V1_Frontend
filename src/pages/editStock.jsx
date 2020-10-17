c7b2d1eb12bb254736d603a61d3cdc6aa3c2b4c5
Marvin <nopparut.bit@hotmail.com>
2020-10-17 16:27:37 +0700

    table pagination add selected

--- src/pages/editStock.jsx
+++ src/pages/editStock.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import TablePagination from "../components/tablePagination";
+import { Box } from "@material-ui/core";
 
 export default function editStock() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <Box
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        height="90vh"
+        alignItems="center"
+      >
+        <Box
+          maxWidth="sm"
+          width="1100px"
+          height="80%"
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+        >
+          <Box
+            width="95%"
+            border="2px solid lightgrey"
+            height="100%"
+            borderRadius="20px"
+            boxShadow="4px 4px 9px -2px rgba(61,61,61,0.43)"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+              <TablePagination></TablePagination>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
